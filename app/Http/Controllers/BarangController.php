d5cff11783e0a5cd496183c531a64c213751e93b
rizkilabs <rizkilabs.dev@gmail.com>
2025-04-19 19:00:04 +0700

    add barang feature

---
+++ app/Http/Controllers/BarangController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barang;
+use Illuminate\Http\Request;
+
+class BarangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $barang = Barang::all();
+        return view('barang.index', compact('barang'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('barang.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Barang::create($request->except('_token'));
+        return redirect('/barang')->with('success', 'Barang ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Barang $barang)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Barang $barang)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Barang $barang)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Barang $barang)
+    {
+        //
+    }
+}
